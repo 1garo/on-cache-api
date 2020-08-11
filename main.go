761e90212c17b023c8553e06a2b2c540ce0be2a1
1garo <alevardai@hotmail.com>
2020-08-11 20:03:08 -0300

    return the user name on message field when hit a POST, and the id when its a GET

--- main.go
+++ main.go
@@ -4,24 +4,40 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
+	"log"
 )
 
 type LOGIN struct {
 	USER     string `json:"user" binding:"required"`
 	PASSWORD string `json:"password" binding:"required"`
+	ID 			 int 		`json:"id"`
 }
 
 func main() {
-  
-	login := []*login{}
+	login := []*LOGIN{}
 	var i int = 0
+	var ids []int
+	log.Printf("%d", ids)
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/api/:id", func(c *gin.Context) {
+		var loginTemp *LOGIN
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			log.Fatalf("Cannot convert the id passed! \n%s", err)
+			
+		}
+		// TODO: Make 400 and 500 handlers
+		if err := c.ShouldBindJSON(&loginTemp); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		log.Printf("%s", c.Request.Write)
 		c.JSON(http.StatusOK, gin.H{
-			"message": login,
+			"message": login[id].ID,
+			})
 		})
-	})
-	r.POST("/foo", func(c *gin.Context) {
+	r.POST("/api", func(c *gin.Context) {
 		var loginTemp *LOGIN
 		c.BindJSON(&loginTemp)
 		login = append(login, loginTemp)
